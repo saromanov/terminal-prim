9a6752a48a7d02bc2a175f0715b8dd74fef385e9
Sergey <xxsmotur@gmail.com>
2019-12-03 22:58:40 +0500

    Adding Text method

--- text.go
+++ text.go
@@ -2,6 +2,7 @@ package prim
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 
 	"github.com/mgutz/ansi"
@@ -46,6 +47,12 @@ func (t *Text) IdentTop(n int) *Text {
 	return t
 }
 
+// Text overwrites current main string
+func (t *Text) Text(str string) *Text {
+	t.output = str
+	return t
+}
+
 func (t *Text) AlignCenter(width int) *Text {
 	totalPadLen := width - len(t.output)
 	if totalPadLen < 0 {
@@ -63,8 +70,11 @@ func (t *Text) Color(color string) *Text {
 }
 
 // Output returns result string
-func (t *Text) Output() string {
-	return output(t)
+func (t *Text) Output() *Text {
+	fmt.Println(output(t))
+	t.output = ""
+	t.outputColor = ""
+	return t
 }
 
 func output(t *Text) string {
