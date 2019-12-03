8663e37d4ea78827f4d67c21b498ee3057eb55d2
Sergey <xxsmotur@gmail.com>
2019-12-03 16:01:57 +0500

    Add color method

--- text.go
+++ text.go
@@ -3,12 +3,15 @@ package prim
 import (
 	"bytes"
 	"strings"
+
+	"github.com/mgutz/ansi"
 )
 
 // Text defines implementation basic things for text
 type Text struct {
-	output string
-	buffer bytes.Buffer
+	output      string
+	buffer      bytes.Buffer
+	outputColor string
 }
 
 func NewText(text string) *Text {
@@ -24,6 +27,10 @@ func (t *Text) IdentLeft(n int) {
 	lines := strings.Split(t.output, "\n")
 	for _, line := range lines {
 		buffer.WriteString(result)
+		if t.outputColor != "" {
+			buffer.WriteString(t.outputColor)
+			continue
+		}
 		buffer.WriteString(line)
 	}
 	t.buffer = buffer
@@ -35,6 +42,10 @@ func (t *Text) IdentTop(n int) {
 	for i := 0; i < n; i++ {
 		t.buffer.WriteString("\n")
 	}
+}
+
+func (t *Text) Color(color string) {
+	t.outputColor = ansi.Color(t.output, color)
 }
 
 // Output returns result string
