3dd3549613a2d18d4cf75a1e97aa8a519b96a4ec
Sergey <xxsmotur@gmail.com>
2019-12-04 21:08:57 +0500

    Adding tests

---
+++ text_test.go
@@ -0,0 +1,13 @@
+package prim
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIdentTop(t *testing.T) {
+	output := NewText("output").
+		IdentTop(2).
+		String()
+	assert.Equal(t, output, "\n\noutput")
+}
