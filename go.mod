495b986ad0992bd460e678a239212ebaece0cb7d
Sergey <xxsmotur@gmail.com>
2019-12-05 19:35:29 +0500

    Add representation of tables

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/cheggaaa/pb v2.0.7+incompatible
 	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b
+	github.com/saromanov/tables v0.0.0-20191205142233-b2f2120d80ac
 	github.com/stretchr/testify v1.4.0
 	gopkg.in/VividCortex/ewma.v1 v1.1.1 // indirect
 	gopkg.in/cheggaaa/pb.v2 v2.0.7 // indirect
---
+++ table.go
@@ -0,0 +1,28 @@
+package prim
+
+import (
+	"github.com/saromanov/tables"
+)
+
+// Table provides representation of the table
+type Table struct {
+	table *tables.App
+}
+
+func NewTable() *Table {
+	return &Table{
+		table: tables.New(),
+	}
+}
+
+func (t *Table) AddHeaders(headers ...interface{}) {
+	t.table.AddHeader(headers)
+}
+
+func (t *Table) AddLines(lines ...interface{}) {
+	t.table.AddLine(lines)
+}
+
+func (t *Table) Output() {
+	t.table.Build()
+}
